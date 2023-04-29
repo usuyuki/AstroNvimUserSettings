2c37f85282492b807924a89cb88b2f86aefd29c2
usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-04-30 01:17:20 +0900

    refactor 無駄な記述除外

--- plugins/php.lua
+++ plugins/php.lua
@@ -1,43 +1,26 @@
-local utils = require "astronvim.utils"
+-- customize mason plugins
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table or string "all".
-      if not opts.ensure_installed then
-        opts.ensure_installed = {}
-      elseif opts.ensure_installed == "all" then
-        return
-      end
-      -- Add the "python" and "toml" language to opts.ensure_installed.
-      utils.list_insert_unique(opts.ensure_installed, "php")
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table.
-      if not opts.ensure_installed then opts.ensure_installed = {} end
-      -- Add go lsps to opts.ensure_installed using vim.list_extend.
-      utils.list_insert_unique(opts.ensure_installed, {"php-cs-fixer", "phpstan",})
-    end,
-  },
+  -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table.
-      if not opts.ensure_installed then opts.ensure_installed = {} end
-      -- Add pyright lsp to opts.ensure_installed using table.insert.
-      utils.list_insert_unique(opts.ensure_installed, {"intelephense"})
-    end,
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = {
+      ensure_installed = { "intelephense" },
+    },
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      ensure_installed = {"php-cr-fixer", "phpstan"},
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table.
-      if not opts.ensure_installed then opts.ensure_installed = {} end
-      -- Add to opts.ensure_installed using table.insert.
-      utils.list_insert_unique(opts.ensure_installed, "php")
-    end,
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = {
+      ensure_installed = { "php" },
+    },
   },
 }
